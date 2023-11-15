fcd40498a7d155bca96400fc99d77d07166e77bf
Andrew Zhang <andrewzhang2323@gmail.com>
2023-11-14 22:23:41 -0600

    Added Login Page

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from "./src/screens/LoginScreen.js"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <LoginScreen/>
+  </View>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
