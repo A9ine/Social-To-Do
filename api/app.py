c1322c1374f224821f5fc94ee44b68e9f89ae44e
Andrew Zhang <andrewzhang2323@gmail.com>
2023-11-27 15:10:05 +0000

    - Finished chat, task, friend endpoints - refined db struct

--- api/app.py
+++ api/app.py
@@ -198,6 +198,328 @@ def changePassword():
     finally:
         conn.close()
 
+# add friend
+@app.route('/addFriend', methods=['POST'])
+def addFriend():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    data = request.get_json()
+    user1 = data.get('user1')
+    user2 = data.get('user2')
+    
+    # Check for missing data
+    if not user1 or not user2:
+        return jsonify({"error": "2 users are required."}), 400
+    
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user1,))
+    user1_id = cursor.fetchone()
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user2,))
+    user2_id = cursor.fetchone()
+
+    if not user1_id or not user2_id:
+        return jsonify({"error": "One or more of the users do not exist"}), 400
+    
+    #check if friendship already exist 
+    cursor.execute("SELECT * FROM friends WHERE(user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)", (user1_id[0], user2_id[0], user2_id[0], user1_id[0]))
+    if cursor.fetchone():
+        return jsonify({"error": "Friendship Already Exists"}), 400
+    
+    #add new friendship
+    cursor.execute("INSERT INTO friends (user_id, friend_id, status, created_at, updated_at) VALUES (?, ?, 'accepted', datetime('now'), datetime('now'))", (user1_id[0], user2_id[0]))
+    conn.commit()
+    return jsonify({"message": "Friendship added sucessfully"}), 200
+
+# delete friend
+@app.route('/deleteFriend', methods=['POST'])
+def deleteFriend():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    data = request.get_json()
+    user1 = data.get('user1')
+    user2 = data.get('user2')
+    
+    # Check for missing data
+    if not user1 or not user2:
+        return jsonify({"error": "2 users are required."}), 400
+    
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user1,))
+    user1_id = cursor.fetchone()
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user2,))
+    user2_id = cursor.fetchone()
+
+    if not user1_id or not user2_id:
+        return jsonify({"error": "One or more of the users do not exist"}), 400
+    
+    cursor.execute("SELECT * FROM friends WHERE(user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?)", (user1_id[0], user2_id[0], user2_id[0], user1_id[0]))
+    if not cursor.fetchone():
+        return jsonify({"error": "Friendship Does Not Exists"}), 400
+    
+    cursor.execute("DELETE FROM friends WHERE (user_id = ? AND friend_id = ?) OR (user_id = ? AND friend_id = ?),(user1_id[0], user2_id[0], user2_id[0], user1_id[0]) ")
+    conn.commit()
+
+    return jsonify({"message": "Friendship deleted sucessfully"}), 200
+
+# get friends
+@app.route('/getFriends', methods=['GET'])
+def getFriends():
+    conn = db_connection()
+    cursor = conn.cursor()
+    user = request.args.get('username')
+
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user,))
+    user_id_record = cursor.fetchone()
+    
+    user_id = user_id_record[0]
+
+    if not user_id:
+        return jsonify({"error": "User Does Not Exist"}), 400
+
+    cursor.execute("""
+        SELECT u.user_id, u.username
+        FROM users u
+        JOIN friends f ON u.user_id = f.friend_id OR u.user_id = f.user_id
+        WHERE (f.user_id = ? OR f.friend_id = ?) AND u.user_id != ?
+    """, (user_id, user_id, user_id))
+
+    friends = cursor.fetchall()
+
+    friend_list = [
+        {
+            'friend_id' : friend[0],
+            'friend_username' : friend[1]
+        }
+        for friend in friends
+    ]
+
+    return jsonify({"friends": friend_list}), 200
+
+
+# add tasks
+@app.route('/addTask', methods=['POST'])
+def addTask():
+    conn = db_connection()
+    cursor = conn.cursor()
+    data = request.get_json()
+    user = data.get('username')
+    task = data.get('task')
+    due_date = data.get("due_date")
+
+    if not user or not task or not due_date:
+        return jsonify({"error": "all fields need to be filled"}), 400
+
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user,))
+    user_id_record = cursor.fetchone()
+    
+    if not user_id_record:
+        return jsonify({"error": "User Does Not Exist"}), 400
+    user_id = user_id_record[0]
+
+    cursor.execute("INSERT INTO tasks(user_id, task, created_at, updated_at, due_date, completed) VALUES (?, ?, datetime('now'), datetime('now'), ?, ?)", (user_id, task, due_date, False))
+    conn.commit()
+    return jsonify({"message": "Task added sucessfully"}), 200
+
+    
+# mark task as done
+
+# retrieve tasks 
+@app.route('/getIncompletedTasks', methods=['GET'])
+def getIncompletedTasks():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    user = request.args.get('username')
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user,))
+    user_id_record = cursor.fetchone()
+    
+    if not user_id_record:
+        return jsonify({"error": "User Does Not Exist"}), 400
+    user_id = user_id_record[0]
+
+    cursor.execute("SELECT * FROM tasks WHERE (user_id = ? AND completed = ?)", (user_id, False))
+    tasks = cursor.fetchall()
+
+    tasks_list = [
+        {
+            'task_id' : task[0],
+            'task_description' : task[2],
+            'due_date' : task[5],
+            'completed': task[6],
+            'created_at' : task[3]
+        }
+        for task in tasks
+    ]
+    
+    return jsonify({"tasks":tasks_list}), 200
+
+
+@app.route('/markTaskAsDone', methods = ['POST'])
+def markTaskAsDone():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    data = request.get_json()
+    task_id = data.get('task_id')
+    task_id = int(task_id)
+    cursor.execute("SELECT * FROM tasks WHERE task_id = ?", (task_id,))
+    task = cursor.fetchone()
+    if not task:
+        return jsonify({"error": "Task Does Not Exist"}), 400
+    
+    cursor.execute("UPDATE tasks SET completed = ? WHERE task_id = ?", (True, task_id))
+    conn.commit()
+
+    return jsonify({'message': "Task marked as done successfully"}), 200
+    
+
+
+@app.route('/getAllTasks', methods=['GET'])
+def getAllTasks():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    user = request.args.get('username')
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(user,))
+    user_id_record = cursor.fetchone()
+    
+    if not user_id_record:
+        return jsonify({"error": "User Does Not Exist"}), 400
+    user_id = user_id_record[0]
+
+    cursor.execute("SELECT * FROM tasks WHERE user_id = ?", (user_id,))
+    tasks = cursor.fetchall()
+
+    tasks_list = [
+        {
+            'task_id' : task[0],
+            'task_description' : task[2],
+            'due_date' : task[5],
+            'completed': task[6],
+            'created_at' : task[3]
+        }
+        for task in tasks
+    ]
+    
+    return jsonify({"tasks":tasks_list}), 200
+
+
+# send chat
+@app.route('/sendChat', methods=['POST'])
+def sendChat():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    data = request.get_json()
+    username = data.get('username')
+    group_id = data.get('group_id')
+    message = data.get('message')
+
+    if not username or not group_id or not message:
+        return jsonify({"error": "Missing Data"}), 400
+    
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(username,))
+    user_id_record = cursor.fetchone()
+    
+    if not user_id_record:
+        return jsonify({"error": "User Does Not Exist"}), 400
+    user_id = user_id_record[0]
+
+    cursor.execute("INSERT INTO chat_messages(group_id, user_id, message) VALUES (?, ?, ?)", (group_id, user_id, message))
+    conn.commit()
+
+    return jsonify({"message": "Chat Sent Successfully"}), 200
+
+
+# Retrieve chat
+@app.route('/getChat', methods=['GET'])
+def getChat():
+    conn = db_connection()
+    cursor = conn.cursor()
+    group_id = request.args.get('group_id')
+    
+    if not group_id:
+        return jsonify({"error": "group_id Does Not Exist"}), 400
+    
+    cursor.execute("SELECT user_id, message, sent_at FROM chat_messages WHERE group_id = ? ORDER BY sent_at DESC", (group_id,))
+    messages = cursor.fetchall()
+
+    usernames = []
+    for message in messages:
+        user_id = message[0]
+        cursor.execute("SELECT username FROM users WHERE user_id = ?",(user_id,))
+        user_id_record = cursor.fetchone()
+        if not user_id_record:
+            return jsonify({"error": "User Does Not Exist"}), 400
+        username = user_id_record[0]
+        usernames.append(username)
+    
+    
+
+    messages_list = [
+        {
+            'user_id' : message[0],
+            'message' : message[1],
+            'sent_at' : message[2]
+        }
+        for message in messages
+    ]
+
+    for i in range(len(messages_list)):
+        messages_list[i]['usernames'] = usernames[i]
+
+    return jsonify({"messages": messages_list}), 200
+
+
+# Start groupchat
+@app.route('/startChat', methods=['POST'])
+def startChat():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    data = request.get_json()
+    group_name = data.get('group_name')
+    created_by = data.get('created_by')
+    members = data.get('members') # list of members
+    print(members)
+
+    if not group_name or not created_by or not members:
+        return jsonify({"error": "Missing Info"}), 400
+
+    cursor.execute("SELECT user_id FROM users WHERE username = ?",(created_by,))
+    created_by_record = cursor.fetchone()
+    
+    if not created_by_record:
+        return jsonify({"error": "User Does Not Exist 1"}), 400
+    created_by = created_by_record[0]
+    
+    cursor.execute("INSERT INTO groups(group_name, created_by, created_at) VALUES (?,?, datetime('now'))", (group_name, created_by))
+    group_id = cursor.lastrowid
+
+    user_ids = []
+
+    for username in members:
+        print("i am:", username)
+        cursor.execute("SELECT user_id FROM users WHERE username = ?",(username,))
+        user_id_record = cursor.fetchone()
+        
+        if not user_id_record:
+            return jsonify({"error": "User Does Not Exist"}), 400
+        user_id = user_id_record[0]
+        user_ids.append(user_id)
+
+    user_ids.append(created_by)
+
+    for user_id in user_ids:
+        cursor.execute("INSERT INTO group_members (group_id, user_id, joined_at) VALUES (?,?, datetime('now'))", (group_id, user_id))
+    
+    conn.commit()
+
+    return jsonify({"message": "Group chat started successfully", "group_id": group_id})
+
+# post 
+# retrieve post
+
 
 if __name__ == '__main__':
     app.run(port=2323)
--- api/database/db.py
+++ api/database/db.py
@@ -27,9 +27,74 @@ create_verification_codes_table = """
     );
 """
 
+# SQL query to create the 'friends' table
+create_friends_table = """
+    CREATE TABLE IF NOT EXISTS friends (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        friend_id INTEGER NOT NULL,
+        status TEXT NOT NULL,
+        created_at TEXT NOT NULL,
+        updated_at TEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        FOREIGN KEY (friend_id) REFERENCES users(user_id)
+    );
+"""
+
+create_tasks_table = """
+    CREATE TABLE IF NOT EXISTS tasks (
+        task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        task TEXT NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        due_date DATETIME NOT NULL,
+        completed BOOLEAN NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    );
+"""
+
+create_groups_table = """
+    CREATE TABLE IF NOT EXISTS groups (
+        group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        group_name INTEGER NOT NULL,
+        created_by INTEGER NOT NULL,
+        created_at DATETIME NOT NULL,
+        FOREIGN KEY (created_by) REFERENCES users(user_id)
+    );
+"""
+
+create_group_members_table = """
+    CREATE TABLE IF NOT EXISTS group_members (
+        group_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        joined_at DATETIME NOT NULL,
+        PRIMARY KEY (group_id, user_id),
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+        FOREIGN KEY (group_id) REFERENCES groups(group_id)
+    );
+"""
+
+create_chat_messages_table = """
+    CREATE TABLE IF NOT EXISTS chat_messages (
+        message_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        group_id INTEGER NOT NULL,
+        user_id INTEGER NOT NULL,
+        message TEXT NOT NULL,
+        sent_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+        FOREIGN KEY (group_id) REFERENCES groups(group_id)
+    );
+"""
+
 # Execute each SQL statement
 cursor.execute(create_users_table)
 cursor.execute(create_verification_codes_table)
+cursor.execute(create_friends_table)
+cursor.execute(create_tasks_table)
+cursor.execute(create_groups_table)
+cursor.execute(create_group_members_table)
+cursor.execute(create_chat_messages_table)
 
 # Commit the changes and close the connection
 conn.commit()
