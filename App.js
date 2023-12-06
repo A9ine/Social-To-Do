55435f06d1f469df3032e90cc9c413f721205563
Huong Thien Do <tdo7@wisc.edu>
2023-12-06 13:05:46 -0600

    Fix the titles for all screens

--- App.js
+++ App.js
@@ -23,20 +23,20 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'LOGIN' }} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ title: 'REGISTER' }} />
+        <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} options={{ title: 'Reset Password' }} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen}  />
         <Stack.Screen name="MakePostScreen" component={MakePostScreen} />
-        <Stack.Screen name="TaskScreen" component={TaskScreen} />
-        <Stack.Screen name="AddTaskScreen" component={AddTaskScreen} />
-        <Stack.Screen name="SettingScreen" component={SettingScreen} />
+        <Stack.Screen name="TaskScreen" component={TaskScreen} options={{ title: 'TASKS' }}/>
+        <Stack.Screen name="AddTaskScreen" component={AddTaskScreen} options={{ title: 'Add Task' }} />
+        <Stack.Screen name="SettingScreen" component={SettingScreen} options={{ title: 'SETTING' }} />
         <Stack.Screen name="AddFriendScreen" component={AddFriendScreen} />
         <Stack.Screen name="FriendsScreen" component={FriendsScreen} />
-        <Stack.Screen name="NotificationsScreen" component={NotificationsScreen} />
+        <Stack.Screen name="NotificationsScreen" component={NotificationsScreen}/>
         <Stack.Screen name="ChatListScreen" component={ChatListScreen} />
-        <Stack.Screen name="StartChatScreen" component={StartChatScreen} />
-        <Stack.Screen name="ChatScreen" component={ChatScreen} />
+        <Stack.Screen name="StartChatScreen" component={StartChatScreen}/>
+        <Stack.Screen name="ChatScreen" component={ChatScreen}/>
         <Stack.Screen name='CommentScreen' component={CommentScreen} />
       </Stack.Navigator>
     </NavigationContainer>
