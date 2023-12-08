5b6525101c8ba094b5813e13b3d046bcb1d6ec0c
Huong Thien Do <tdo7@wisc.edu>
2023-12-07 18:11:26 -0600

    Fix the delete button in TaskScreen.js

--- src/screens/TaskScreen.js
+++ src/screens/TaskScreen.js
@@ -287,7 +287,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: 80, 
-    height: 80,
+    height: 75,
     marginLeft: -25
   },
   deleteButtonIcon: {
